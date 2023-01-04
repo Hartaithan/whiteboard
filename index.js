23c4bc992209e619a9f445196139939dfd99047d
hartaithan <hartaithan@gmail.com>
2023-01-04 14:11:29 +0900

    fix server imports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 require("dotenv").config();
